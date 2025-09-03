a86b5e2752c6e70188f5e44b28b2e870729ecba2
Osahon Okoro <ookoro2@byupathway.edu>
2025-09-03 20:58:25 +0100

    my first commit

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,22 +1,19 @@
 using System;
 
-namespace bond;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-             // Prompt the user for their first name
-            Console.Write("What is your first name? ");
-            string firstName = Console.ReadLine();
+        // Prompt the user for their first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompt the user for their last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Display the formatted output
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+    }
+}    
     
-            // Prompt the user for their last name
-            Console.Write("What is your last name? ");
-            string lastName = Console.ReadLine();
-    
-            // Display the formatted output
-            Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
-        }
-    }    
-    
-}
