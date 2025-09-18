1c6939917fba5ccdee349de9a287e0786ff44a63
Osahon Okoro <ookoro2@byupathway.edu>
2025-09-18 16:40:18 +0100

    Updated journal logic and resolved merge conflicts

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
-class Program
+public class Journal
 {
-    static void Main(string[] args)
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        _entries.Add(entry);
+    }
+
+    public void DisplayEntries()
+    {
+        if (_entries.Count == 0)
+        {
+            Console.WriteLine("No journal entries yet.");
+            return;
+        }
+
+        Console.WriteLine("----- Journal Entries -----");
+        foreach (Entry entry in _entries)
+        {
+            Console.WriteLine(entry.ToString());
+            Console.WriteLine("---------------------------");
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                // Use | as separator to avoid issues with commas
+                writer.WriteLine($"{entry.Date}|{entry.Prompt}|{entry.Response}");
+            }
+        }
+
+        Console.WriteLine($"Journal saved to '{filename}'.");
     }
 }
